58da43a503a18ac7fe3d1aff70c15850a80b4e9e
Andrei Șaramet <andreisaramet21@gmail.com>
2023-07-27 13:41:56 +0300

    assignment for-loops

--- JavaBasics/src/LogicalOp.java
+++ JavaBasics/src/LogicalOp.java
@@ -1,4 +1,5 @@
 public class LogicalOp {
+    //    If-else 3
     public int checkBiggerNumber(final int a, final int b) {
         if (a > b) {
             return a;
@@ -14,6 +15,7 @@ public class LogicalOp {
 //        return a > b ? a : b;
     }
 
+    //    If-else 4
     public String compare(final String string) {
         if ("Evozon".equals(string)) {
             return "Learning text comparison";
@@ -21,6 +23,7 @@ public class LogicalOp {
         return "Got to try some more";
     }
 
+    //    If-else 5
     public String compare(final String string, final int number) {
         if ("Evozon".equals(string) && number <= 3) {
             return String.format("%s, %d", string, number);
@@ -31,6 +34,7 @@ public class LogicalOp {
         return "No condition matched";
     }
 
+    //    If-else 6
     public String snowLevel(final int amount) {
         if (amount > 8 || amount == 6) {
             return String.format("The amount of snow this winter was(cm): %d", amount);
@@ -38,6 +42,7 @@ public class LogicalOp {
         return String.format("The forecast snow is(cm): %d", amount);
     }
 
+    //    If-else 7
     public String checkCondition(final double number) {
         if (number > 3 && number != 4) {
             return "The number is greater than 3 and not equal to 4";
@@ -48,6 +53,7 @@ public class LogicalOp {
         return "The number is lower than 3";
     }
 
+    //    If-else 8
     public void checkNumber(final int number) {
         switch (number) {
             case 1:
@@ -99,6 +105,7 @@ public class LogicalOp {
 //        });
     }
 
+    //    If-else 9
     public boolean isNumberEven(final int number) {
         if (number % 2 == 0) {
             return true;
@@ -108,6 +115,7 @@ public class LogicalOp {
 //        return number % 2 == 0;
     }
 
+    //    If-else 10
     public boolean isEligibleToVote(final double age) {
         if (age >= 18) {
             return true;
@@ -117,6 +125,7 @@ public class LogicalOp {
 //        return age >= 18;
     }
 
+    //    If-else 11
     public double max(final double a, final double b, final double c) {
         if (a > b) {
             if (a > c) {
@@ -128,5 +137,116 @@ public class LogicalOp {
             return b;
         }
         return c;
+    }
+
+    //    For-loops 1.
+    public void countUp(int number) {
+        if (number <= 100) {
+            for (; number <= 100; number++) {
+                System.out.printf("%d ", number);
+            }
+        } else {
+            for (; number >= 100; number--) {
+                System.out.printf("%d ", number);
+            }
+        }
+        System.out.println();
+    }
+
+    //    For-loops 2
+    public void countDown(int number) {
+        if (number >= -100) {
+            for (; number >= -100; number--) {
+                System.out.printf("%d ", number);
+            }
+        } else {
+            for (; number <= -100; number++) {
+                System.out.printf("%d ", number);
+            }
+        }
+        System.out.println();
+    }
+
+    //    For-loops 3
+    public void countFromTo(int start, int end) {
+        if (start <= end) {
+            for (; start <= end; start++) {
+                System.out.printf("%d ", start);
+            }
+        } else {
+            for (; start >= end; start--) {
+                System.out.printf("%d ", start);
+            }
+        }
+        System.out.println();
+    }
+
+    //    For-loops 4
+    public void countIncreseFromTo(int start, int end) {
+        if (end < start) {
+            final int cpy = start;
+            start = end;
+            end = cpy;
+        }
+        for (; start <= end; start++) {
+            System.out.printf("%d ", start);
+        }
+        System.out.println();
+    }
+
+    //    For-loops 5
+    public void displayEven1To100() {
+//        As 1 is not an even number, we start displaying from 2
+        int start = 2;
+        for (; start <= 100; start += 2) {
+            System.out.printf("%d ", start);
+        }
+        System.out.println();
+    }
+
+    //    For-loops 6
+    public void displayOdd1To100() {
+        int start = 1;
+        for (; start <= 100; start += 2) {
+            System.out.printf("%d ", start);
+        }
+        System.out.println();
+    }
+
+    //    For-loops 7
+    public int sumTo100(int number) {
+        if (number > 100) {
+            System.out.printf("No sum can be computed from %d to 100\n", number);
+            return -1;
+        }
+        int sum = 0;
+        for (; number <= 100; number++) {
+            sum += number;
+        }
+        return sum;
+//        a shorter alternative using sum of consecutive numbers from 1 to 100 and from 1 to number
+//        return (100 * 101 - (number - 1) * number) / 2;
+    }
+
+    //    For-loops 8
+    public double meanTo100(int number) {
+        if (number > 100) {
+            System.out.printf("No mean can be computed from %d to 100\n", number);
+            return -1;
+        }
+        return (double) this.sumTo100(number) / (100 - number + 1);
+    }
+
+    //    For-loops 9
+    public void displayPattern() {
+        int start = 7;
+        for (; start > 0; start--) {
+            int length = start;
+            final StringBuilder line = new StringBuilder();
+            for (; length > 0; length--) {
+                line.append("*");
+            }
+            System.out.println(line);
+        }
     }
 }
--- JavaBasics/src/Main.java
+++ JavaBasics/src/Main.java
@@ -1,102 +1,88 @@
 public class Main {
-/*
-    Assignment - if-else
+    /*
+    Assignment - For loops
 
-    Rezolvati urmatoarele exercitii in aceiasi solutie "JavaBasics":
+    Rezolvati urmatoarele exercitii in proiectul de Calculator folosind bucle FOR:
 
-    1. Creati o clasa Calculator, in care sa mutati din main toate metodele care tin de aceasta clasa (adunare, scadere, etc...). Dupa ce toate metodele au fost mutate, testati ca inca functioneaza, apelandu-le in clasa Main, si metoda main().
+    1. Creati o metoda in clasa LogicalOp, care sa primeasca un numar ca si parametru. Metoda sa verifice numarul primit, si sa afiseze in consola o numaratoare pana la 100, pornind de la numarul primit ca si parametru. Atentie la crearea tipului metodei, deoarece metoda afiseaza, nu returneaza.
 
-    Pentru asta, va trebui sa va creati un obiect de clasa Calculator, in metoda main(), si de pe obiect sa apelati fiecare metoda.
+     Apelati metoda in clasa Main, metoda main() pentru a verifica daca functioneaza.
 
-    Tineti minte, doar metoda main() are drept de rulare, deci doar ce punem in ea se va excecuta. Ea apeleaza restul metodelor.
+    2. Creati o metoda in clasa LogicalOp, care sa primeasca un numar ca si parametru. Metoda sa verifice numrul primit, si sa afiseze in consola o numaratoare pana la -100, pornind de la numarul primit ca si parametru. Atentie la crearea tipului metodei, deoarece metoda afiseaza, nu returneaza.
 
-    Atentie: Dupa ce ati mutat metodele, sa scoateti cuvantul static din definitia metodelor. Doar metodele din clasa Main ce metoda main() le apeleaza direct vor ramane statice.
+     Apelati metoda in clasa Main, metoda main() pentru a verifica daca functioneaza.
 
-    2. Creati o clasa LogicalOp, in care sa rezolvati exercitiile de mai jos.
+    3. Creati o metoda in clasa LogicalOp, care sa primeasca doi parametrii de tip numar. Metoda sa afiseze o numaratoare intre cele doua numere primite, pornind de la primul parametru primit, pana la cel de-al doilea(ex: daca int xe primul parametru iar int y  este cel de-al doilea, numaratoarea sa se faca de la x, la y.
 
-    3. In clasa LogicalOp, creati o metoda de tip int, care sa se numeasca checkBiggerNumber(), si sa primeasca doua int-uri ca si parametrii. Folosind if - else, verificati in interiorul metode care numar este mai mare, dintre cele doua valori care va vin ca si parametrii, si returnati numarul mai mare.
+    Atentie la crearea tipului metodei, deoarece metoda afiseaza, nu returneaza.
 
-    Pentru a testa aceasta metoda, mergeti in metoda main(), din clasa Main, si construiti un obiect de tip LogicalOp, si apelati metoda cu valori. Fiind o metoda de tip int, ea va returna o valoare, pe care noi trebuie sa o prindem si ca sa o vedem, trebuie sa o afisam in consola.
+    Apelati metoda in clasa Main, metoda main() pentru a verifica daca functioneaza.
 
-    4. Creati o metoda de tip String, care sa primeasca un parametru de tip String. Verificati daca textul ce vine ca si parametru este egal cu “Evozon”. Daca da, atunci metoda sa returneze “Learning text comparison”. Daca nu, atunci metoda sa returneze “Got to try some more”.
+    4. Creati o metoda in clasa LogicalOp, care sa primeasca doi parametrii de tip numar. Metoda trebuie sa verifice care dintre cele doua numere este mai mare, si sa execute o numaratoare crescatoare de la cel mai mic la cel mai mare.(ex: daca int x e primul parametru iar int y  este cel de-al doilea, daca x  este mai mare decat y,  atunci numaratoarea sa fie de la y la x).
 
-    Indiciu 1: Textul nu se compara cu ==,  ci cu .equals().
+    Atentie la crearea tipului metodei, deoarece metoda afiseaza, nu returneaza.
 
-    Indiciu 2: .equals() compara si literele mici si cele mari, si le vede diferite. (adica “test” nu este egal cu “Test”).
+    Apelati metoda in clasa Main, metoda main() pentru a verifica daca functioneaza.
 
-    Apelati metoda in metoda main() pentru a vedea daca functioneaza.
+    5. Creati o metoda in clasa LogicalOp care sa afiseze toate numerele pare de la 1 la 100.
+    6. Creati o metoda in clasa LogicalOp care sa afiseze toate numerele impare de la 1 la 100.
 
-    Atentie: se va folosi obiectul de LogicalOp creat pentru exercitiul de mai sus. Pentru a apela mai multe metode din aceeasi clasa e suficient sa se construiasca un singur obiect de clasa respectiva.
+    7. Creati o metoda in clasa LogicalOp care sa primeasca un parametru de tip numar. Metoda sa adune toate numerele pana la 100, pornind de la numarul primit ca si parametru, iar la final sa returneze rezultatul. Apelati metoda in clasa Main, metoda main() pentru a verifica daca functioneaza.
 
-    5. Creati o metoda de tip String, care sa primeasca un parametru de tip String, si un parametru de tip int. Daca textul primit ca si parametru este egal cu “Evozon”, si numarul primit ca si parametru este mai mic sau egal cu 3, returnati textul si numarul, in ordinea asta. Daca textul nu este “Evozon” si numarul este mai mare sau egal cu 4, returnati numarul si textul, in ordinea asta. Apelati metoda in main() pentru a verifica daca functioneaza.
 
-    6. Creati o metoda de tip String, care sa primeasca un parametru de tip int. Daca numarul primit este mai mare de 8, SAU egal cu 6, atunci sa se returneze “The amount of snow this winter was(cm): “ si numarul. Daca nu, sa returneze “The forecast snow is(cm):” si numarul. Apelati metoda in main() pentru a verifica daca functioneaza.
+    8. Creati o metoda in clasa LogicalOp care sa primeasca un parametru de tip numar. Metoda sa adune toate numerele pana la 100, pornind de la numarul primit ca si parametru, si sa calculeze media numerelor. La final, metoda sa returneze media. Apelati metoda in clasa Main, metoda main() pentru a verifica daca functioneaza.
 
-    7. Creati o metoda de tip String, care sa primeasca un parametru de tip numar. Daca numarul e mai mare decat 3, SI nu egal cu 4, returnati “The number is greater than 3 and not equal to 4”. Daca numarul este egal cu 4, returnati “The number is equal to 4”, iar daca numarul este mai mic de 3 returnati “The number is lower than 3”. Apelati metoda in main() pentru a verifica daca functioneaza.
+    9. Creat o metoda in clasa LogicalOp care sa afiseze urmatorul tipar:
 
-    8. Creati o metoda care primeste un numar ca si parametru si sa afiseze ce numar a primit. Folosind constructia Switch-Case, verificati ce numar s-a primit si afisati textul "The number is:  x !" unde x trebuie sa reprezinte numarul apasat. Pentru exemplul de fata sa nu se foloseasca concatenarea stringului "The number is:" cu numarul de la parametru, ci sa se scrie intreg textul cu tot cu numar pentru fiecare caz din Switch-Case. Apelati metoda in main() pentru a verifica daca functioneaza.
+    *******
 
-    9. Creati o metoda care sa se numeasca isNumberEven. Metoda trebuie sa primeasca ca parametru un numar, si sa returneze adevarat sau fals. Daca numarul e par sa returneze true iar daca e impar sa returnese false. Apelati metoda in main() pentru a verifica daca functioneaza.
+    ******
 
-    10. Creati o metoda care sa se numeasca isEligibleToVote. Metoda sa primeasca ca si parametru un numar, care sa reprezinte varsta, iar metoda sa iti zica daca daca persoana are drept de vot. Daca varsta e peste 18, sa se returneze true iar daca nu, sa returneze false. Apelati metoda in main() pentru a verifica daca functioneaza.
+    *****
 
-    11. Creati o metoda care sa primeasca 3 numere ca si parametrii si sa returneze cel mai mare numar. Apelati metoda in main() pentru a verifica daca functioneaza.
+    ****
+
+    ***
+
+    **
+
+    *
+
+    Cateva mentiuni:
+    - pentru a putea rezolva problema, trebuie folosite doua bucle for. O bucla se va construi in alta bucla.
+
+    - prima bucla for se va ocupa de afisarea pe orizontala, si cealalta pe verticala
+
+    - semnul asterisk (*) se poate afisa sub forma de string
+
+    Apelati metoda in clasa Main, metoda main() pentru a verifica daca functioneaza
      */
     public static void main(String[] args) {
-//         1.
-        int a, b;
-        final Calculator calculator = new Calculator();
-        a = 5;
-        b = 7;
-        System.out.printf("%d + %d = %d\n", a, b, calculator.add(a, b));
-        a = 129;
-        b = 319;
-        System.out.printf("%d - %d = %d\n", a, b, calculator.subtract(a, b));
-        a = 2;
-        b = 8;
-        System.out.printf("%d * %d = %d\n", a, b, calculator.multiply(a, b));
-        a = 81;
-        b = 9;
-        System.out.printf("%d / %d = %f\n", a, b, calculator.divide(a, b));
-        System.out.println(calculator.mean(4, 7, 12));
-        System.out.println(calculator.remainder(5, 3));
-//        3.
+//        1.
         LogicalOp op = new LogicalOp();
-        System.out.printf("The bigger number is: %d\n", op.checkBiggerNumber(8, 1));
+        op.countUp(101);
+        op.countUp(15);
+//        2.
+        op.countDown(-101);
+        op.countDown(15);
+//        3.
+        op.countFromTo(3, 19);
+        op.countFromTo(100, 99);
 //        4.
-        System.out.println(op.compare("evoZon"));
-        System.out.println(op.compare("Evozon"));
+        op.countIncreseFromTo(2, 7);
+        op.countIncreseFromTo(87, 76);
 //        5.
-        System.out.println(op.compare("Evozon", 1));
-        System.out.println(op.compare("Evozon", 4));
-        System.out.println(op.compare("evozon", 4));
-        System.out.println(op.compare("evozon", 1));
+        op.displayEven1To100();
 //        6.
-        System.out.println(op.snowLevel(15));
-        System.out.println(op.snowLevel(7));
-        System.out.println(op.snowLevel(6));
+        op.displayOdd1To100();
 //        7.
-        System.out.println(op.checkCondition(1));
-        System.out.println(op.checkCondition(4));
-        System.out.println(op.checkCondition(Math.PI));
+        System.out.println(op.sumTo100(47));
+        System.out.println(op.sumTo100(101));
 //        8.
-        op.checkNumber(4);
-        op.checkNumber(0);
-        op.checkNumber(10);
-        op.checkNumber(1);
-        op.checkNumber(11);
+        System.out.println(op.meanTo100(47));
+        System.out.println(op.meanTo100(101));
 //        9.
-        System.out.println(op.isNumberEven(5));
-        System.out.println(op.isNumberEven(12));
-//        10.
-        System.out.println(op.isEligibleToVote(18));
-//        11.
-        System.out.println(op.max(1, 2, 3));
-        System.out.println(op.max(1, 3, 2));
-        System.out.println(op.max(2, 1, 3));
-        System.out.println(op.max(2, 3, 1));
-        System.out.println(op.max(3, 1, 2));
-        System.out.println(op.max(3, 2, 1));
+        op.displayPattern();
     }
 
 
