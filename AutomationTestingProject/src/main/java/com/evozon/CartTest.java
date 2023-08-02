2d2125022f5645da2fa49af981e73b32ecee1107
Andrei Șaramet <andreisaramet21@gmail.com>
2023-08-02 15:57:12 +0300

    refactor add to cart test

--- AutomationTestingProject/src/main/java/com/evozon/CartTest.java
+++ AutomationTestingProject/src/main/java/com/evozon/CartTest.java
@@ -10,11 +10,21 @@ public class CartTest {
     public void validAddToCartTest() {
         final WebDriver driver = new ChromeDriver();
         driver.get("http://qa1magento.dev.evozon.com/");
-        driver.findElement(By.cssSelector("#header > div > div.skip-links > div > a > span.label")).click();
-        driver.findElement(By.cssSelector("#header-account > div > ul > li.last > a")).click();
-        driver.findElement(By.cssSelector("#email")).sendKeys("john@email.com");
-        driver.findElement(By.cssSelector("#pass")).sendKeys("password");
-        driver.findElement(By.cssSelector("#send2 > span > span")).click();
+
+        new LoginTest().loginDriver(driver, "john@email.com", "password");
+
+        this.addToCartDriver(driver);
+
+        final String expected = String.format("%s was added to your shopping cart.", "Chelsea Tee");
+        if (driver.findElement(By.cssSelector("body > div > div > div.main-container.col1-layout > div > div > div.cart.display-single-price > ul > li > ul > li > span")).getText().equals(expected)) {
+            System.out.println("Success");
+        } else {
+            System.err.println("Fail");
+        }
+        driver.close();
+    }
+
+    public void addToCartDriver(WebDriver driver) {
         final WebElement category = driver.findElement(By.cssSelector("#nav > ol > li.level0.nav-2.parent > a"));
         Actions action = new Actions(driver);
         action.moveToElement(category).perform();
@@ -23,12 +33,5 @@ public class CartTest {
         driver.findElement(By.cssSelector("#swatch27 > span.swatch-label > img")).click();
         driver.findElement(By.cssSelector("#swatch80 > span.swatch-label")).click();
         driver.findElement(By.cssSelector("#product_addtocart_form > div.product-shop > div.product-options-bottom > div.add-to-cart > div.add-to-cart-buttons > button > span > span")).click();
-        final String expected = String.format("%s was added to your shopping cart.", "Chelsea Tee");
-        if (driver.findElement(By.cssSelector("body > div > div > div.main-container.col1-layout > div > div > div.cart.display-single-price > ul > li > ul > li > span")).getText().equals(expected)) {
-            System.out.println("Success");
-        } else {
-            System.err.println("Fail");
-        }
-        driver.close();
     }
 }
