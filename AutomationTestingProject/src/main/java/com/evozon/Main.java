44417c565c2e7cab52eb6d019dcfb02182be55b0
Andrei Șaramet <andreisaramet21@gmail.com>
2023-08-02 13:18:38 +0300

    automation testing project add to wishlist test

--- AutomationTestingProject/src/main/java/com/evozon/Main.java
+++ AutomationTestingProject/src/main/java/com/evozon/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) throws Exception {
          System.setProperty("webdriver.chrome.driver", "resources/chromedriver");
 //         new RegisterTest().validRegisterTest();
-        new LoginTest().validLoginTest();
+//        new LoginTest().validLoginTest();
+        new WishlistTest().validAddToWishlistTest();
     }
 }
