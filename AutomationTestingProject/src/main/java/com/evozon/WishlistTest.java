41e974b3711b01e15e0c7ae28016ee43bc558f33
Andrei Șaramet <andreisaramet21@gmail.com>
2023-08-02 15:49:17 +0300

    refactor add to wishlist test

--- AutomationTestingProject/src/main/java/com/evozon/WishlistTest.java
+++ AutomationTestingProject/src/main/java/com/evozon/WishlistTest.java
@@ -11,16 +11,11 @@ public class WishlistTest {
         final WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("http://qa1magento.dev.evozon.com");
-        driver.findElement(By.cssSelector("#header > div > div.skip-links > div > a > span.label")).click();
-        driver.findElement(By.cssSelector("#header-account > div > ul > li.last > a")).click();
-        driver.findElement(By.cssSelector("#email")).sendKeys("john@email.com");
-        driver.findElement(By.cssSelector("#pass")).sendKeys("password");
-        driver.findElement(By.cssSelector("#send2 > span > span")).click();
-        final WebElement category = driver.findElement(By.cssSelector("#nav > ol > li.level0.nav-2.parent > a"));
-        Actions action = new Actions(driver);
-        action.moveToElement(category).perform();
-        driver.findElement(By.cssSelector("#nav > ol > li.level0.nav-2.parent > ul > li.level1.nav-2-1.first > a")).click();
-        driver.findElement(By.cssSelector("body > div > div > div.main-container.col3-layout > div > div.col-wrapper > div.col-main > div.category-products > ul > li:nth-child(2) > div > div.actions > ul > li:nth-child(1) > a")).click();
+
+        new LoginTest().loginDriver(driver, "john@email.com", "password");
+
+        this.addToWishlistDriver(driver);
+
         final String expected = String.format("%s has been added to your wishlist. Click here to continue shopping.", "Chelsea Tee");
         if (driver.findElement(By.cssSelector("body > div > div > div.main-container.col2-left-layout > div > div.col-main > div.my-account > div.my-wishlist > ul > li > ul > li > span")).getText().equals(expected)) {
             System.out.println("Success!");
@@ -28,5 +23,13 @@ public class WishlistTest {
             System.err.println("Fail");
         }
         driver.close();
+    }
+
+    public void addToWishlistDriver(WebDriver driver) {
+        final WebElement category = driver.findElement(By.cssSelector("#nav > ol > li.level0.nav-2.parent > a"));
+        Actions action = new Actions(driver);
+        action.moveToElement(category).perform();
+        driver.findElement(By.cssSelector("#nav > ol > li.level0.nav-2.parent > ul > li.level1.nav-2-1.first > a")).click();
+        driver.findElement(By.cssSelector("body > div > div > div.main-container.col3-layout > div > div.col-wrapper > div.col-main > div.category-products > ul > li:nth-child(2) > div > div.actions > ul > li:nth-child(1) > a")).click();
     }
 }
