b7625be7622d229facefe592f544b4e1e7221bf7
Andrei Șaramet <andreisaramet21@gmail.com>
2023-08-02 15:44:28 +0300

    refactor login test

--- AutomationTestingProject/src/main/java/com/evozon/LoginTest.java
+++ AutomationTestingProject/src/main/java/com/evozon/LoginTest.java
@@ -7,12 +7,13 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class LoginTest {
     public void validLoginTest() {
         final WebDriver driver = new ChromeDriver();
+        final String email = "john@email.com";
+        final String password = "password";
+
         driver.get("http://qa1magento.dev.evozon.com");
-        driver.findElement(By.cssSelector("#header > div > div.skip-links > div > a > span.label")).click();
-        driver.findElement(By.cssSelector("#header-account > div > ul > li.last > a")).click();
-        driver.findElement(By.cssSelector("#email")).sendKeys("john@email.com");
-        driver.findElement(By.cssSelector("#pass")).sendKeys("password");
-        driver.findElement(By.cssSelector("#send2 > span > span")).click();
+
+        this.loginDriver(driver, email, password);
+
         final String expected = "Hello, John Smith!";
         if (driver.findElement(By.cssSelector("body > div > div > div.main-container.col2-left-layout > div > div.col-main > div.my-account > div > div.welcome-msg > p.hello > strong")).getText().equals(expected)) {
             System.out.println("Success");
@@ -20,5 +21,13 @@ public class LoginTest {
             System.err.println("Fail");
         }
         driver.close();
+    }
+
+    public void loginDriver(final WebDriver driver, final String email, final String password) {
+        driver.findElement(By.cssSelector("#header > div > div.skip-links > div > a > span.label")).click();
+        driver.findElement(By.cssSelector("#header-account > div > ul > li.last > a")).click();
+        driver.findElement(By.cssSelector("#email")).sendKeys(email);
+        driver.findElement(By.cssSelector("#pass")).sendKeys(password);
+        driver.findElement(By.cssSelector("#send2 > span > span")).click();
     }
 }
