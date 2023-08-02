eb339a7eb96a5e94914a44bc755d205ad6d96699
Andrei Șaramet <andreisaramet21@gmail.com>
2023-08-02 15:52:43 +0300

    refactor search test

--- AutomationTestingProject/src/main/java/com/evozon/SearchTest.java
+++ AutomationTestingProject/src/main/java/com/evozon/SearchTest.java
@@ -9,8 +9,9 @@ public class SearchTest {
     public void validSearchNoResultTest() {
         final WebDriver driver = new ChromeDriver();
         driver.get("http://qa1magento.dev.evozon.com");
-        driver.findElement(By.cssSelector("#search")).sendKeys("efygreuifwyugefvg");
-        driver.findElement(By.cssSelector("#search")).sendKeys(Keys.RETURN);
+
+        this.searchDriver(driver, "efygreuifwyugefvg");
+
         final String expected = "You're search returns no results.";
         if (driver.findElement(By.cssSelector("body > div > div > div.main-container.col3-layout > div > div.col-wrapper > div.col-main > p")).getText().equals(expected)) {
             System.out.println("Success");
@@ -20,12 +21,12 @@ public class SearchTest {
         driver.close();
     }
 
-
     public void validSearchWithResultsTest() {
         final WebDriver driver = new ChromeDriver();
         driver.get("http://qa1magento.dev.evozon.com");
-        driver.findElement(By.cssSelector("#search")).sendKeys("shirt");
-        driver.findElement(By.cssSelector("#search")).sendKeys(Keys.RETURN);
+
+        this.searchDriver(driver, "shirt");
+
         final String expected = String.format("%d Item(s)", 5);
         if (driver.findElement(By.cssSelector("body > div > div > div.main-container.col3-layout > div > div.col-wrapper > div.col-main > div.category-products > div.toolbar > div.pager > div > p > strong")).getText().equals(expected)) {
             System.out.println("Success");
@@ -33,5 +34,10 @@ public class SearchTest {
             System.err.println("Fail");
         }
         driver.close();
+    }
+
+    public void searchDriver(WebDriver driver, String keywords) {
+        driver.findElement(By.cssSelector("#search")).sendKeys(keywords);
+        driver.findElement(By.cssSelector("#search")).sendKeys(Keys.RETURN);
     }
 }
