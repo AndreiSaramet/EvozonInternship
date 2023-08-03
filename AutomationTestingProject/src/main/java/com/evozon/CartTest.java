c998587d8620bbe86ddfd5f5a296bfaeeefd504e
Andrei Șaramet <andreisaramet21@gmail.com>
2023-08-03 11:30:17 +0300

    add to cart changed css selectors

--- AutomationTestingProject/src/main/java/com/evozon/CartTest.java
+++ AutomationTestingProject/src/main/java/com/evozon/CartTest.java
@@ -5,6 +5,10 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class CartTest {
     public void validAddToCartTest() {
@@ -12,11 +16,12 @@ public class CartTest {
         driver.get("http://qa1magento.dev.evozon.com/");
 
         new LoginTest().loginDriver(driver, "john@email.com", "password");
+        final String productName = "Park Avenue Pleat Front Trousers";
 
-        this.addToCartDriver(driver);
+        this.addToCartDriver(driver, productName);
 
-        final String expected = String.format("%s was added to your shopping cart.", "Chelsea Tee");
-        if (driver.findElement(By.cssSelector("body > div > div > div.main-container.col1-layout > div > div > div.cart.display-single-price > ul > li > ul > li > span")).getText().equals(expected)) {
+        final String expected = String.format("%s was added to your shopping cart.", productName);
+        if (driver.findElement(By.cssSelector(".success-msg span")).getText().equals(expected)) {
             System.out.println("Success");
         } else {
             System.err.println("Fail");
@@ -24,14 +29,14 @@ public class CartTest {
         driver.close();
     }
 
-    public void addToCartDriver(WebDriver driver) {
-        final WebElement category = driver.findElement(By.cssSelector("#nav > ol > li.level0.nav-2.parent > a"));
+    public void addToCartDriver(final WebDriver driver, final String productName) {
+        final WebElement category = driver.findElement(By.cssSelector("li.nav-1 > a"));
         Actions action = new Actions(driver);
         action.moveToElement(category).perform();
-        driver.findElement(By.cssSelector("#nav > ol > li.level0.nav-2.parent > ul > li.level1.nav-2-1.first > a")).click();
-        driver.findElement(By.cssSelector("body > div > div > div.main-container.col3-layout > div > div.col-wrapper > div.col-main > div.category-products > ul > li:nth-child(2) > div > div.actions > a")).click();
-        driver.findElement(By.cssSelector("#swatch27 > span.swatch-label > img")).click();
-        driver.findElement(By.cssSelector("#swatch79 > span.swatch-label")).click();
-        driver.findElement(By.cssSelector("#product_addtocart_form > div.product-shop > div.product-options-bottom > div.add-to-cart > div.add-to-cart-buttons > button > span > span")).click();
+        driver.findElement(By.cssSelector("li.nav-1-3 > a")).click();
+        new WebDriverWait(driver, Duration.ofSeconds(30)).until(ExpectedConditions.elementToBeClickable(By.cssSelector(String.format("li:not(.odd) .product-image[title=\"%s\"]", productName)))).click();
+        driver.findElement(By.cssSelector(".option-taupe img")).click();
+        driver.findElement(By.cssSelector(".option-6")).click();
+        driver.findElement(By.cssSelector(".add-to-cart-buttons .btn-cart")).click();
     }
 }
