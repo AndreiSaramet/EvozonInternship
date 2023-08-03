66167661e1fa4683255e34437c22b9ebabc92674
Andrei Șaramet <andreisaramet21@gmail.com>
2023-08-03 16:18:50 +0300

    smoke test suite

---
+++ AutomationTestingProject/src/test/java/com/evozon/SmokeTestSuite.java
@@ -0,0 +1,9 @@
+package com.evozon;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({RegisterTest.class, LoginTest.class, CartTest.class, CheckoutTest.class})
+public class SmokeTestSuite {
+}
