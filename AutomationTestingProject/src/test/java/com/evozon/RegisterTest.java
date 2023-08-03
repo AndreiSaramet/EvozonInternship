ab6b33f120b530d54adea6153f4a4eb9ac50558f
Andrei Șaramet <andreisaramet21@gmail.com>
2023-08-03 18:53:24 +0300

    register test back to random

--- AutomationTestingProject/src/test/java/com/evozon/RegisterTest.java
+++ AutomationTestingProject/src/test/java/com/evozon/RegisterTest.java
@@ -7,16 +7,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.time.Duration;
-import java.util.List;
+import java.util.Random;
 
 @RunWith(JUnit4.class)
 public class RegisterTest extends BaseTest {
@@ -32,38 +26,11 @@ public class RegisterTest extends BaseTest {
 
     @Test
     public void validRegisterTest() {
-        final String firstName = "elton";
-        final String middleName = "Michael";
-        final String lastName = "John";
-        final String email = "elton@john.com";
+        final String firstName = this.generateString(10);
+        final String middleName = this.generateString(10);
+        final String lastName = this.generateString(10);
         final String password = "password";
-
-        this.driver.get("http://qa1magento.dev.evozon.com/admin");
-        final String adminUsername = "testuser";
-        final String adminPassword = "password123";
-        this.driver.findElement(By.cssSelector("#username")).sendKeys(adminUsername);
-        this.driver.findElement(By.cssSelector("#login")).sendKeys(adminPassword);
-        this.driver.findElement(By.cssSelector(".form-button")).click();
-        this.driver.findElement(By.cssSelector(".message-popup-head a")).click();
-        new Actions(this.driver).moveToElement(this.driver.findElement(By.cssSelector("#nav > li:nth-child(4) > a"))).perform();
-        this.driver.findElement(By.cssSelector("#nav > li:nth-child(4) > ul > li:first-of-type")).click();
-        this.driver.findElement(By.cssSelector("[id=\"customerGrid_filter_email\"]")).sendKeys(email, Keys.RETURN);
-
-        new WebDriverWait(this.driver, Duration.ofSeconds(60)).until(ExpectedConditions.stalenessOf(this.driver.findElement(By.cssSelector("#customerGrid_table"))));
-
-        List<? extends WebElement> elements = this.driver.findElement(By.cssSelector("#customerGrid_table")).findElements(By.cssSelector("#customerGrid_table tbody tr td:not(.empty-text)"));
-        elements.stream()
-                .filter(el -> email.equals(el.findElement(By.cssSelector("td:nth-child(4)")).getText()))
-                .findAny()
-                .ifPresent(customer -> {
-                    customer.findElement(By.cssSelector("td a")).click();
-                    this.driver.findElement(By.cssSelector(".main-col [title=\"Delete Customer\"]")).click();
-                    this.driver.switchTo().alert().accept();
-                });
-
-        this.driver.get("http://qa1magento.dev.evozon.com");
-        driver.findElement(By.cssSelector(".skip-account")).click();
-        driver.findElement(By.cssSelector("a[title=Register]")).click();
+        final String email = String.format("%s%s", this.generateString(5), "@email.com");
 
         driver.findElement(By.cssSelector("#firstname")).sendKeys(firstName);
         driver.findElement(By.cssSelector("#middlename")).sendKeys(middleName);
@@ -77,6 +44,14 @@ public class RegisterTest extends BaseTest {
         final String expected = String.format("Welcome, %s %s %s!", firstName, middleName, lastName).toUpperCase();
         final String actual = this.driver.findElement(By.cssSelector("p.welcome-msg")).getText();
         Assert.assertEquals(expected, actual);
+    }
+
+    private String generateString(final Integer length) {
+        return new Random().ints()
+                .filter(i -> (i >= 'a' && i <= 'z') || (i >= 'A' && i <= 'Z'))
+                .limit(length)
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                .toString();
     }
 
     @After
