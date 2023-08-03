f6f3e818209b887dc0176b7b0b84d6d9df7e5e34
Andrei Șaramet <andreisaramet21@gmail.com>
2023-08-03 10:29:05 +0300

    register test changed css selectors

--- AutomationTestingProject/src/main/java/com/evozon/RegisterTest.java
+++ AutomationTestingProject/src/main/java/com/evozon/RegisterTest.java
@@ -13,9 +13,12 @@ public class RegisterTest {
         final String lastName = "John";
         final String password = "johnny";
         final String email = "elton.john@email.com";
+
         registerDriver(driver, firstName, middleName, lastName, email, password);
+
         final String expected = String.format("Welcome, %s %s %s!", firstName, middleName, lastName).toUpperCase();
-        if (driver.findElement(By.cssSelector("body > div > div > div.header-language-background > div > p")).getText().equals(expected)) {
+
+        if (driver.findElement(By.cssSelector("p.welcome-msg")).getText().equals(expected)) {
             System.out.println("Success");
         } else {
             System.err.println("Fail");
@@ -24,15 +27,15 @@ public class RegisterTest {
     }
 
     private static void registerDriver(final WebDriver driver, final String firstName, final String middleName, final String lastName, final String email, final String password) {
-        driver.findElement(By.cssSelector("#header > div > div.skip-links > div > a > span.label")).click();
-        driver.findElement(By.cssSelector("#header-account > div > ul > li:nth-child(5) > a")).click();
+        driver.findElement(By.cssSelector(".skip-account")).click();
+        driver.findElement(By.cssSelector("a[title=Register]")).click();
         driver.findElement(By.cssSelector("#firstname")).sendKeys(firstName);
         driver.findElement(By.cssSelector("#middlename")).sendKeys(middleName);
         driver.findElement(By.cssSelector("#lastname")).sendKeys(lastName);
         driver.findElement(By.cssSelector("#email_address")).sendKeys(email);
         driver.findElement(By.cssSelector("#password")).sendKeys(password);
         driver.findElement(By.cssSelector("#confirmation")).sendKeys(password);
-        driver.findElement(By.cssSelector("#form-validate > div.fieldset > ul > li.control > label")).click();
-        driver.findElement(By.cssSelector("#form-validate > div.buttons-set > button > span > span")).click();
+        driver.findElement(By.cssSelector("#is_subscribed")).click();
+        driver.findElement(By.cssSelector("button[title=Register]")).click();
     }
 }
