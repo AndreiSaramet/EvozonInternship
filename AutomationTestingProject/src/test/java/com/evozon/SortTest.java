f629cb1f5da5cea2804861c88b52d25ec3d1c847
Andrei Șaramet <andreisaramet21@gmail.com>
2023-08-03 15:36:27 +0300

    sort test convert to junit

--- AutomationTestingProject/src/test/java/com/evozon/SortTest.java
+++ AutomationTestingProject/src/test/java/com/evozon/SortTest.java
@@ -1,5 +1,9 @@
 package com.evozon;
 
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -10,80 +14,72 @@ import java.text.NumberFormat;
 import java.text.ParseException;
 import java.util.Locale;
 import java.util.function.BiPredicate;
-import java.util.function.Consumer;
 
 public class SortTest {
+    private WebDriver driver;
 
+    @Before
+    public void beforeEach() {
+        this.driver = new ChromeDriver();
+        this.driver.get("http://qa1magento.dev.evozon.com/");
+    }
+
+    @Test
     public void sortByPriceAscTest() {
-        this.sortByTest(driver -> this.sortByDriver(driver, "Price"),
-                driver -> this.comparePrices(driver, (a, b) -> a <= b));
+        this.sortByDriver("Price");
+        this.comparePrices((a, b) -> a <= b);
     }
 
+    @Test
     public void sortByPriceDscTest() {
-        this.sortByTest(driver -> {
-                    this.sortByDriver(driver, "Price");
-                    this.sortDsc(driver);
-                },
-                driver -> this.comparePrices(driver, (a, b) -> a >= b));
+        this.sortByDriver("Price");
+        this.sortDsc();
+        this.comparePrices((a, b) -> a >= b);
     }
 
+    @Test
     public void sortByNameAscTest() {
-        this.sortByTest(driver -> this.sortByDriver(driver, "Name"),
-                driver -> this.compareNames(driver, (a, b) -> a.compareTo(b) <= 0));
+        this.sortByDriver("Name");
+        this.compareNames((a, b) -> a.compareTo(b) <= 0);
     }
 
+    @Test
     public void sortByNameDscTest() {
-        this.sortByTest(driver -> {
-            this.sortByDriver(driver, "Name");
-            this.sortDsc(driver);
-        }, driver -> this.compareNames(driver, (a, b) -> a.compareTo(b) >= 0));
+        this.sortByDriver("Name");
+        this.sortDsc();
+        this.compareNames((a, b) -> a.compareTo(b) >= 0);
     }
 
-    private void sortByTest(final Consumer<WebDriver> sortFunction,
-                            final Consumer<WebDriver> compareFunction) {
-        final WebDriver driver = new ChromeDriver();
-        driver.get("http://qa1magento.dev.evozon.com/");
-
-        sortFunction.accept(driver);
-
-        compareFunction.accept(driver);
-
-        driver.close();
+    @After
+    public void afterEach() {
+        this.driver.close();
     }
 
-    public void comparePrices(final WebDriver driver, final BiPredicate<Double, Double> compare) {
+    private void comparePrices(final BiPredicate<Double, Double> compare) {
         NumberFormat format = NumberFormat.getInstance(Locale.UK);
         final String price1String = driver.findElement(By.cssSelector("body > div > div.page > div.main-container.col3-layout > div > div.col-wrapper > div.col-main > div.category-products > ul > li:nth-child(1) > div > div.price-box")).getText().substring(1);
         final String price2String = driver.findElement(By.cssSelector("body > div > div.page > div.main-container.col3-layout > div > div.col-wrapper > div.col-main > div.category-products > ul > li:nth-child(2) > div > div.price-box")).getText().substring(1);
         try {
-            if (compare.test(format.parse(price1String).doubleValue(), format.parse(price2String).doubleValue())) {
-                System.out.println("Success");
-            } else {
-                System.err.println("Fail");
-            }
+            Assert.assertTrue(compare.test(format.parse(price1String).doubleValue(), format.parse(price2String).doubleValue()));
         } catch (ParseException nfe) {
-            throw new RuntimeException(nfe);
+            Assert.fail(nfe.toString());
         }
     }
 
-    public void compareNames(final WebDriver driver, final BiPredicate<String, String> compare) {
+    private void compareNames(final BiPredicate<String, String> compare) {
         final String name1 = driver.findElement(By.cssSelector("body > div > div.page > div.main-container.col3-layout > div > div.col-wrapper > div.col-main > div.category-products > ul > li:nth-child(2) > div > h2")).getText();
         final String name2 = driver.findElement(By.cssSelector("body > div > div.page > div.main-container.col3-layout > div > div.col-wrapper > div.col-main > div.category-products > ul > li:nth-child(7) > div > h2")).getText();
-        if (compare.test(name1, name2)) {
-            System.out.println("Success");
-        } else {
-            System.err.println("Fail");
-        }
+        Assert.assertTrue(compare.test(name1, name2));
     }
 
-    public void sortByDriver(final WebDriver driver, final String option) {
+    private void sortByDriver(final String option) {
         new Actions(driver).moveToElement(driver.findElement(By.cssSelector("#nav > ol > li.level0.nav-3.parent > a"))).perform();
         driver.findElement(By.cssSelector("#nav > ol > li.level0.nav-3.parent > ul > li.level1.nav-3-2 > a")).click();
         new Select(driver.findElement(By.cssSelector("body > div > div.page > div.main-container.col3-layout > div > div.col-wrapper > div.col-main > div.category-products > div.toolbar > div.sorter > div > select"))).selectByVisibleText(option);
     }
 
 
-    public void sortDsc(final WebDriver driver) {
+    private void sortDsc() {
         driver.findElement(By.cssSelector("body > div > div.page > div.main-container.col3-layout > div > div.col-wrapper > div.col-main > div.category-products > div.toolbar > div.sorter > div > a")).click();
     }
 }
